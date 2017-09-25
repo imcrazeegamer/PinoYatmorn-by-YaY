0efb4de7acc67868a208aa271f18e51e19d8c4ad
Imcrazeegamer <imcrazeegamer@gmail.com>
2017-09-25 20:39:59 +0300

    Built the game into an exe file & added dialog
    PinoYatmorn-by-YaY\Pino Yatmorn\Builds\Windows\X64\PinoYatmornX64_0.0.0.exe

---
+++ Pino Yatmorn/Assets/Pino Yatmorn/Scripts/DialogManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour {
+
+    public Text nameText;
+    public Text dialogText;
+    public Animator animator;
+
+
+    Queue<string> sentences;
+    // Use this for initialization
+    void Start() {
+        sentences = new Queue<string>();
+
+    }
+    public void StartDialog(Dialog dialog)
+    {
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialog.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialog.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return null;
+        }
+    }
+    void EndDialog()
+    {
+        Debug.Log("Dialog has ended");
+        animator.SetBool("IsOpen", false);
+    }
+}
